cfbf3e9d4c6a803ee334911d935b674e5a5ee012
Tomas <vrbato02@jcu.cz>
2024-10-13 15:08:56 +0000

    .

--- second.py
+++ second.py
@@ -2,20 +2,17 @@ def cislo_text(cislo):
     # funkce zkonvertuje cislo do jeho textove reprezentace
     # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
     # 13/10/2024 VRBAT - fction implementation
-    if cislo == "": #kontrola, zda je vyplněn vstup
-        print("Číslo nebylo zadáno.")
-        exit
-    elif cislo is not int: #kontrola dat. typu
-        print("Špatný datový typ")
-        exit
-    elif cislo > 100 or cislo < 0: #kontrola intervalu <0;100>
+       
+    if cislo > 100 or cislo < 0: #kontrola intervalu <0;100>
         print("Vyberte prosím číslo od 0 do 100")
         exit
-    else: #kontroly ok, provedení
-        return "dvacet pět"
+    else:
+        def najdi_text(cislo)
+            if cislo is 
+        #return "dvacet pět"
 
 
 if __name__ == "__main__":
-    cislo = input("Zadej číslo: ")
+    cislo = int(input("Zadej číslo: ")) #kontrola, vstup pouze celé číslo
     text = cislo_text(cislo)
     print(text)
