61b9be48ab0feb9603e74e310534cc1e64c25805
Tomas <vrbato02@jcu.cz>
2024-10-13 14:27:58 +0000

    ,

--- second.py
+++ second.py
@@ -2,12 +2,15 @@ def cislo_text(cislo):
     # funkce zkonvertuje cislo do jeho textove reprezentace
     # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
     # 13/10/2024 VRBAT - fction implementation
-    if cislo is None: 
-        raise Exception("Číslo nebylo zadáno.")
+    if cislo is "": 
+        print("Číslo nebylo zadáno.")
+        exit
     elif cislo is not int: 
-        raise Exception("Špatný datový typ")
+        print("Špatný datový typ")
+        exit
     elif cislo > 100 or cislo < 0: 
-        raise Exception("Vyberte prosím číslo od 0 do 100")
+        print("Vyberte prosím číslo od 0 do 100")
+        exit
     else:
         return "dvacet pět"
 
