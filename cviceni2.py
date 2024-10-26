24e922ab817b745f93a7ec47a28a84f3e6bad9dd
Tomas <vrbato02@jcu.cz>
2024-10-26 15:26:49 +0000

    ano

--- cviceni2.py
+++ cviceni2.py
@@ -1,8 +1,22 @@
- def faktorial(n):
+def faktorial(n):
     if n == 1:
         return 1
-     else:
+    else:
         return n * faktorial(n - 1)
 
+def faktorial_while(n):
+    while n >= 1:
+
+    return vysledek
+
+def faktorial_for(n):
+    vysledek = 1
+    for x in range(1, n + 1):
+        vysledek: *= x
+    return vysledek
 
 if __name__ == "__main__":
+    print(faktorial_while)
+    n = 5
+    x = range(1, n+1)
+    #print(list(x))
