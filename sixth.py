ce961c3e19806c2049f85404ffc9af6822bb19fb
Tomas <vrbato02@jcu.cz>
2024-12-06 16:45:14 +0000

    Hotovo

--- sixth.py
+++ sixth.py
@@ -4,6 +4,7 @@ from lxml import html
 from unittest.mock import MagicMock
 
 #12/06/2024 VRBAT
+#jak otestovat: do terminálu zada: /home/codespace/.python/current/bin/python /workspaces/pg/sixth.py https://www.seznam.cz/
 def download_url_and_get_all_hrefs(url):
     """
     Funkce stahne url predanou v parametru url pomoci volani response = requests.get(),
