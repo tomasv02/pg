957ddf8c7c4e0453856529f345297bbcdc46fc4f
Tomas <vrbato02@jcu.cz>
2025-04-15 08:14:15 +0000

    ..

--- print_invoice/myapp/models.py
+++ print_invoice/myapp/models.py
@@ -10,26 +10,27 @@ from django.db import models
 from django.db import models
 
 class delivery_item(models.Model):
-    delivery_number = models.CharField(max_length=20)  # Např. číslo dodávky
-    delivery_item = models.CharField(max_length=4)  # Popis položky
-    material = models.CharField(max_length=5)  # Materiál
-    material_text = models.CharField(max_length=40)  # Textová poznámka k materiálu
-    delivered_qty = models.IntegerField()  # Množství dodané
-    delivered_qty_unit = models.CharField(max_length=2, default="KS")  # Jednotka množství
-    price_unit_no_dph = models.DecimalField(max_digits=10, decimal_places=3, default=0)  # Cena bez DPH
-    price_unit_currency = models.CharField(max_length=10, default="CZK")  # Měna ceny
-    delivery_item_created_on = models.CharField(max_length=20)  # Datum vytvoření položky
-    delivery_item_created_at = models.CharField(max_length=20)  # Čas vytvoření položky
-    delivery_item_created_by = models.CharField(max_length=12)  # Kdo položku vytvořil
+    delivery_number = models.CharField(max_length=20)  # NEunikátní
+    delivery_item = models.CharField(max_length=4)
+    material = models.CharField(max_length=5)
+    material_text = models.CharField(max_length=40)
+    delivered_qty = models.IntegerField()
+    delivered_qty_unit = models.CharField(max_length=2, default="KS")
+    price_unit_no_dph = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    price_unit_currency = models.CharField(max_length=6, default="CZK")
+    delivery_item_created_on = models.CharField(max_length=20)
+    delivery_item_created_at = models.CharField(max_length=20)
+    delivery_item_created_by = models.CharField(max_length=12)
 
     def __str__(self):
         return f"Delivery {self.delivery_number} - {self.delivery_item}"
-    
+
+
 class delivery_header(models.Model):
-    delivery_number = models.CharField(max_length=20, unique=True)
+    delivery_number = models.CharField(max_length=20)
     delivery_type = models.CharField(max_length=2)
-    invoice_number = models.CharField(max_length=20, blank=True, null=True)
-    invoice_currency = models.CharField(max_length=20)
+    invoice_number = models.IntegerField(default=0)  
+    invoice_currency = models.CharField(max_length=6)
     due_date_invoice = models.CharField(max_length=20)
     customer = models.CharField(max_length=30)
     delivery_date = models.CharField(max_length=20)
